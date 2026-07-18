e6e9a329c60f1c96be7c6d62712783e3991eb7c0
twyora <77087199+twyora@users.noreply.github.com>
2026-07-18 14:36:58 +0800

    feat(multi-image): try converting vvic image to byte array

--- app/src/main/java/io/github/twyora/douyinenhancer/hook/DouyinPackage.kt
+++ app/src/main/java/io/github/twyora/douyinenhancer/hook/DouyinPackage.kt
@@ -85,6 +85,7 @@ class DouyinPackage(private val classLoader: ClassLoader, context: Context) {
     val commentExtensionsKt = CommentExtensionsKtModule()
     val listenerProviderParam = ListenerProviderParamModule()
     val commentImageSaveDownloadListener = CommentImageSaveDownloadListenerModule()
+    val imageResourceRxDownloadListener = ImageResourceRxDownloadListenerModule()
     val downloadInfo = DownloadInfoModule()
     val digestUtils = DigestUtilsModule()
     val ugFileUtils = UGFileUtilsKtModule()
@@ -251,6 +252,20 @@ class DouyinPackage(private val classLoader: ClassLoader, context: Context) {
         )
 
         fun listenerProviderParam() = Field(hookInfo.commentImageSaveDownloadListener.listenerProviderParam.nameOrNull)
+    }
+
+    inner class ImageResourceRxDownloadListenerModule {
+        val selfClass by weak {
+            hookInfo.imageResourceRxDownloadListener.class_.nameOrNull
+                ?.toClass(classLoader)
+        }
+
+        fun onSuccessed() = Method(
+            hookInfo.imageResourceRxDownloadListener.onSuccessed.nameOrNull,
+            hookInfo.imageResourceRxDownloadListener.onSuccessed.parameters.valuesListOrNull
+        )
+
+        fun observableEmitter() = Field(hookInfo.imageResourceRxDownloadListener.observableEmitter.nameOrNull)
     }
 
     inner class DownloadInfoModule {
@@ -448,6 +463,11 @@ class DouyinPackage(private val classLoader: ClassLoader, context: Context) {
         fun images() = Field(hookInfo.aweme.images.nameOrNull)
 
         fun statistics() = Field(hookInfo.aweme.statistics.nameOrNull)
+
+        fun getAid() = Method(
+            hookInfo.aweme.getAid.nameOrNull,
+            hookInfo.aweme.getAid.parameters.valuesListOrNull
+        )
     }
 
     inner class AwemeStatisticsModule {
@@ -1504,6 +1524,9 @@ class DouyinPackage(private val classLoader: ClassLoader, context: Context) {
                     statistics = field {
                         name = "statistics"
                     }
+                    getAid = method {
+                        name = "getAid"
+                    }
                 }
 
                 awemeStatistics = awemeStatistics {
@@ -1803,6 +1826,91 @@ class DouyinPackage(private val classLoader: ClassLoader, context: Context) {
                                 getVisibilityMethod.parameterTypes.forEach { paramType ->
                                     values.add(paramType.name)
                                 }
+                            }
+                        }
+                    }.onFailure {
+                        YLog.error("$TAG: Unable to populate config", it)
+                    }
+                }
+
+                imageResourceRxDownloadListener = imageResourceRxDownloadListener {
+                    runCatching {
+                        val imageDownloadObservableSubscribeMethodData = bridge.findMethod {
+                            matcher {
+                                params {
+                                    add("io.reactivex.ObservableEmitter")
+                                }
+                                usingStrings {
+                                    add("image_music_resource_1")
+                                }
+                                invokeMethods {
+                                    add {
+                                        descriptor =
+                                            "Lcom/ss/android/socialbase/downloader/model/DownloadTask;->monitorScene(Ljava/lang/String;)Lcom/ss/android/socialbase/downloader/model/DownloadTask;"
+                                    }
+                                }
+                            }
+                        }.singleOrNull()
+                        if (imageDownloadObservableSubscribeMethodData == null) {
+                            YLog.error(
+                                "$TAG: Unable to populate ${this::class.java.enclosingClass?.simpleName} config, possibly due to unfound obfuscated methods"
+                            )
+                            return@imageResourceRxDownloadListener
+                        }
+
+                        val imgResRxDownloadListenerList = imageDownloadObservableSubscribeMethodData.invokes.filter {
+                            it.methodName == "<init>" && it.paramTypeNames.contains("io.reactivex.ObservableEmitter")
+                        }.map {
+                            it.className
+                        }.distinct()
+                        if (imgResRxDownloadListenerList.isEmpty() || imgResRxDownloadListenerList.size > 1) {
+                            if (imgResRxDownloadListenerList.isEmpty()) {
+                                YLog.error(
+                                    "$TAG: Unable to populate ${this::class.java.enclosingClass?.simpleName} config, image resource rx download listener not found"
+                                )
+                            } else {
+                                YLog.error(
+                                    "$TAG: Unable to populate ${this::class.java.enclosingClass?.simpleName} config, multiple image resource rx download listener candidates found, don't know which to pick"
+                                )
+                            }
+                            return@imageResourceRxDownloadListener
+                        }
+
+                        val imageResRxDownloadListenerName = imgResRxDownloadListenerList.first()
+                        val imageResRxDownloadListenerOnSuccessedMethod =
+                            imageResRxDownloadListenerName.toClass(hostAppClassLoader).resolve().optional().firstMethodOrNull {
+                                name = "onSuccessed"
+                                parameters(
+                                    "com.ss.android.socialbase.downloader.model.DownloadInfo"
+                                )
+                            }?.self
+                        val imageResRxDownloadListenerObservableEmitter =
+                            imageResRxDownloadListenerName.toClass(hostAppClassLoader).resolve().optional().firstFieldOrNull {
+                                type = "io.reactivex.ObservableEmitter"
+                            }?.self
+
+                        if (imageResRxDownloadListenerOnSuccessedMethod == null || imageResRxDownloadListenerObservableEmitter == null) {
+                            YLog.error(
+                                "$TAG: Unable to populate ${this::class.java.enclosingClass?.simpleName} config, possibly due to unfound obfuscated methods or fields"
+                            )
+                            return@imageResourceRxDownloadListener
+                        }
+
+                        class_ = class_ {
+                            name = imageResRxDownloadListenerName
+                        }
+                        observableEmitter = field {
+                            name = imageResRxDownloadListenerObservableEmitter.name
+                        }
+                        onSuccessed = method {
+                            name = imageResRxDownloadListenerOnSuccessedMethod.name
+                            parameters = MethodKt.parameters {
+                                values.clear()
+                                values.addAll(
+                                    imageResRxDownloadListenerOnSuccessedMethod.parameterTypes.map {
+                                        it.name
+                                    }
+                                )
                             }
                         }
                     }.onFailure {
--- app/src/main/java/io/github/twyora/douyinenhancer/hook/feed/FeedMultiImageHooker.kt
+++ app/src/main/java/io/github/twyora/douyinenhancer/hook/feed/FeedMultiImageHooker.kt
@@ -1,5 +1,10 @@
 package io.github.twyora.douyinenhancer.hook.feed
 
+import android.graphics.Bitmap
+import android.graphics.BitmapFactory
+import android.icu.text.RelativeDateTimeFormatter
+import android.os.strictmode.UntaggedSocketViolation
+import com.highcapable.kavaref.KavaRef.Companion.resolve
 import com.highcapable.yukihookapi.hook.core.YukiMemberHookCreator
 import com.highcapable.yukihookapi.hook.entity.YukiBaseHooker
 import com.highcapable.yukihookapi.hook.log.YLog
@@ -7,14 +12,22 @@ import io.github.twyora.douyinenhancer.config.FastKVConfigManager
 import io.github.twyora.douyinenhancer.config.key.SaveKey
 import io.github.twyora.douyinenhancer.hook.DouyinPackage
 import io.github.twyora.douyinenhancer.hook.HookOnMainProcess
+import io.github.twyora.douyinenhancer.utils.Field
+import io.github.twyora.douyinenhancer.utils.Method
 import io.github.twyora.douyinenhancer.utils.getField
+import io.github.twyora.douyinenhancer.utils.getStaticField
 import io.github.twyora.douyinenhancer.utils.invokeMethod
 import io.github.twyora.douyinenhancer.utils.resolveMethod
 import io.github.twyora.douyinenhancer.utils.setField
+import org.apache.commons.collections4.queue.CircularFifoQueue
+import java.io.FileInputStream
+import java.nio.file.Files
+import kotlin.io.path.Path
 
 @HookOnMainProcess
 object FeedMultiImageHooker : YukiBaseHooker() {
     private val TAG = this::class.simpleName
+    private val ring = CircularFifoQueue<String>(5)
 
     override fun onHook() {
         if (!FastKVConfigManager.settings.getBoolean(SaveKey.FEED_MULTI_IMAGE_REMOVE_WATERMARK, false)) {
@@ -23,6 +36,65 @@ object FeedMultiImageHooker : YukiBaseHooker() {
 
         installInjectPlayUrlIntoImageDownloadHook()
         installDisableSaveImageToVideoLocalWaterMaskHook()
+
+        val packageInstance = DouyinPackage.instance
+        packageInstance.imageResourceRxDownloadListener.selfClass?.resolveMethod(
+            packageInstance.imageResourceRxDownloadListener.onSuccessed()
+        )?.hook {
+            before {
+                val dlInfo = args[0] ?: return@before
+
+                val filePath = dlInfo.invokeMethod<String>(
+                    packageInstance.downloadInfo.getTargetFilePath()
+                )
+                if (filePath == null) {
+                    YLog.error("$TAG: target file path is null")
+                } else {
+                    YLog.debug("$TAG: file path: $filePath")
+                }
+            }
+        }
+
+        //LX/19xB;->LIZIZ(LX/19u9;)Z
+        "X.19xB".toClass().resolve().firstMethodOrNull {
+            name = "LIZIZ"
+        }?.hook {
+            before {
+                val _19u9 = args[0] ?: return@before
+                val imgFilePath = _19u9.getField<String>(
+                    Field(name = "LIZJ")
+                )
+                if (imgFilePath == null) {
+                    YLog.error("$TAG: imgFilePath is null")
+                    return@before
+                } else {
+                    YLog.debug("$TAG: imgFilePath: $imgFilePath")
+                }
+
+                val imgBytes = FileInputStream(imgFilePath).use {
+                    it.readBytes()
+                }
+                val options = BitmapFactory.Options()
+                options.inPreferredConfig = Bitmap.Config.ARGB_8888
+
+                val bitmap = "com.bytedance.fresco.heif.HeifDecoder".toClass().getStaticField<Any>(
+                    Field(name = "sBitmapFactory")
+                )?.invokeMethod<Bitmap>(
+                    Method(name = "decodeByteArray", parameters = null),
+                    imgBytes,
+                    0,
+                    imgBytes.size,
+                    options
+                )
+                if (bitmap == null) {
+                    YLog.error("$TAG: bitmap is null")
+                    return@before
+                } else {
+                    YLog.info("$TAG: bitmap nooooooooooot null!")
+                }
+                bitmap.recycle()
+            }
+        }
     }
 
     private fun installInjectPlayUrlIntoImageDownloadHook(): YukiMemberHookCreator.MemberHookCreator.Result? {
@@ -42,6 +114,15 @@ object FeedMultiImageHooker : YukiBaseHooker() {
                     return@before
                 }
 
+                val awemeId = aweme.invokeMethod<String>(
+                    packageInstance.aweme.getAid()
+                )
+                if (ring.contains(awemeId)) {
+                    return@before
+                } else if (awemeId != null) {
+                    ring.add(awemeId)
+                }
+
                 val awemeImages = aweme.getField<List<*>>(
                     packageInstance.aweme.images()
                 )
@@ -52,7 +133,7 @@ object FeedMultiImageHooker : YukiBaseHooker() {
 
                 awemeImages.forEach { imageStruct ->
                     if (imageStruct == null) {
-                        return@forEach
+                        return@before
                     }
 
                     val urlList = imageStruct.getField<List<*>>(
@@ -61,12 +142,55 @@ object FeedMultiImageHooker : YukiBaseHooker() {
                     if (urlList.isNullOrEmpty()) {
                         YLog.error("$TAG: image has no play URL, skipping watermark-free injection")
                         return@forEach
+                    } else {
+                        YLog.debug("$TAG: image.urlList[0]: ${urlList.first()}")
                     }
 
                     imageStruct.setField(
                         packageInstance.imageUrlStruct.downloadUrlList(),
                         urlList
                     )
+                    /*
+                    val downloadUrlList = imageStruct.getField<List<String>>(
+                        Field(name = "downloadUrlList")
+                    )
+                    val maskUrlList = imageStruct.getField<List<String>>(
+                        Field(name = "maskUrlList")
+                    )
+                    val urlList = imageStruct.getField<List<String>>(
+                        Field(name = "urlList")
+                    )
+                    val watermarkFreeDownloadUrlList = imageStruct.getField<List<String>>(
+                        Field(name = "watermarkFreeDownloadUrlList")
+                    )
+                    val imageExtra = imageStruct.getField<Any>(
+                        Field(name = "imageExtra")
+                    )
+                    val backupUrlList = imageExtra?.getField<List<String>>(
+                        Field(name = "backupUrlList")
+                    )
+
+                    YLog.debug(
+                        "$TAG: urlList: ${
+                            urlList?.firstOrNull()
+                        }, downloadUrlList: ${
+                            downloadUrlList?.firstOrNull()
+                        }, maskUrlList: ${
+                            maskUrlList?.firstOrNull()
+                        }, watermarkFreeDownloadUrlList: ${
+                            watermarkFreeDownloadUrlList?.firstOrNull()
+                        }, backupUrlList: ${
+                            backupUrlList?.firstOrNull()
+                        }"
+                    )
+                     */
+
+                    /*
+                    if (imageStruct == null) {
+                        return@forEach
+                    }
+
+                     */
                 }
             }
         }?.result {
