cea8197fd62edbc63c65ae21651145bfcf4770ab
twyora <77087199+twyora@users.noreply.github.com>
2026-08-06 19:43:10 +0800

    refactor(feed): rename ListenAwemeFeedHooker to ListenAwemeFilterHooker

--- app/src/main/java/io/github/twyora/douyinenhancer/hook/feed/ListenAwemeFilterHooker.kt
+++ app/src/main/java/io/github/twyora/douyinenhancer/hook/feed/ListenAwemeFilterHooker.kt
@@ -12,7 +12,7 @@ import io.github.twyora.douyinenhancer.utils.getField
 import io.github.twyora.douyinenhancer.utils.resolveMethod
 
 @HookOnMainProcess
-object ListenAwemeFeedHooker : YukiBaseHooker() {
+object ListenAwemeFilterHooker : YukiBaseHooker() {
     private val TAG = this::class.simpleName
 
     private val packageInstance
