79a138cdc7cb4752b0353a12c4f38f119bf6f4ef
twyora <77087199+twyora@users.noreply.github.com>
2026-08-08 00:06:15 +0800

    feat(feed-double-tap-digg-hooker): add preference switch for double-tap digg

--- app/src/main/java/io/github/twyora/douyinenhancer/config/key/FeedKey.kt
+++ app/src/main/java/io/github/twyora/douyinenhancer/config/key/FeedKey.kt
@@ -2,4 +2,5 @@ package io.github.twyora.douyinenhancer.config.key
 
 object FeedKey {
     const val BYPASS_LISTEN_AWEME_RESTRICTION = "bypass_listen_aweme_restriction"
+    const val FEED_DOUBLE_TAP_DIGG = "disable_feed_double_tap_digg"
 }
--- app/src/main/java/io/github/twyora/douyinenhancer/hook/feed/FeedDoubleTapDiggHooker.kt
+++ app/src/main/java/io/github/twyora/douyinenhancer/hook/feed/FeedDoubleTapDiggHooker.kt
@@ -3,6 +3,7 @@ package io.github.twyora.douyinenhancer.hook.feed
 import com.highcapable.yukihookapi.hook.entity.YukiBaseHooker
 import com.highcapable.yukihookapi.hook.log.YLog
 import io.github.twyora.douyinenhancer.config.FastKVConfigManager
+import io.github.twyora.douyinenhancer.config.key.FeedKey
 import io.github.twyora.douyinenhancer.config.key.ModuleKey
 import io.github.twyora.douyinenhancer.hook.DouyinPackage
 import io.github.twyora.douyinenhancer.hook.HookOnMainProcess
@@ -18,6 +19,13 @@ object FeedDoubleTapDiggHooker : YukiBaseHooker() {
         get() = !FastKVConfigManager.module.getBoolean(ModuleKey.DISABLE_VERBOSE_LOGS, false)
 
     override fun onHook() {
+        if (!FastKVConfigManager.settings.getBoolean(FeedKey.FEED_DOUBLE_TAP_DIGG, false)) {
+            if (verbose) {
+                YLog.debug("$TAG: double-tap digg disabling disabled, skip hook")
+            }
+            return
+        }
+
         packageInstance.baseListFragmentPanel.selfClass?.resolveMethod(
             packageInstance.baseListFragmentPanel.handleDoubleClick()
         )?.hook {
