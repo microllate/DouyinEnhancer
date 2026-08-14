4cbdd42be3bfab7aaf460ef6ae737d4cbc18fb54
twyora <77087199+twyora@users.noreply.github.com>
2026-08-15 00:25:14 +0800

    feat(danmaku-view-hooker): add switch to configure keeping danmaku visible in clean mode

--- app/src/main/java/io/github/twyora/douyinenhancer/config/key/FeedKey.kt
+++ app/src/main/java/io/github/twyora/douyinenhancer/config/key/FeedKey.kt
@@ -4,4 +4,5 @@ object FeedKey {
     const val BYPASS_LISTEN_AWEME_RESTRICTION = "bypass_listen_aweme_restriction"
     const val FEED_DOUBLE_TAP_DIGG = "disable_feed_double_tap_digg"
     const val FEED_DOUBLE_TAP_OPEN_COMMENT = "feed_double_tap_open_comment"
+    const val KEEP_DANMAKU_VISIBLE = "keep_danmaku_visible"
 }
